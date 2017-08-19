57146059c4c8b2e02cee70fb1387cb5c7596d70f
Ron Gierlach <rongierlach@gmail.com>
2017-08-19 12:38:03 -0500

    ErrorTemplate example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const Web3Provider = require('./Web3Provider');
+const ErrorTemplate = require('./ErrorTemplate');
 
 module.exports.Web3Provider = Web3Provider;
+module.exports.ErrorTemplate = ErrorTemplate;
 
 module.exports = {
-  Web3Provider
+  Web3Provider,
+  ErrorTemplate
 };
